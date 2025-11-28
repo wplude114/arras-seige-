5e4808c99ca632dd8c1f4d0229ba38e133d568a3
wplude114 <wplude@icloud.com>
2025-11-28 13:22:00 -0600

    Update config.js
    pls

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "https://arras-seige.onrender.com",
+    host: "arras-seige.onrender.com",
 
     // Which port to run the web server on.
     port: 8080,

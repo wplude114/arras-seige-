b966565d9e851789b6fa09b3d1a65bc0415b1856
wplude114 <wplude@icloud.com>
2025-11-28 15:10:23 -0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     host: "arras-seige.onrender.com",
 
     // Which port to run the web server on.
-    port:3230,
+    port:3030,
 
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.

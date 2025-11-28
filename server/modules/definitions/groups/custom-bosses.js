be46ec490dcce585fda44fd2800518070590d183
wplude114 <wplude@icloud.com>
2025-11-28 17:46:08 -0600

    Create custom-bosses.js
    for new bosses

---
+++ server/modules/definitions/groups/custom-bosses.js
@@ -0,0 +1,18 @@
+const { combineStats, skillSet, makeAuto, addAura, LayeredBoss, makeDeco, weaponArray, setTurretProjectileRecoil } = require('../facilitators.js');
+const { base, smshskl } = require('../constants.js');
+const g = require('../gunvals.js');
+require('./generics.js');
+require('./tanks.js');
+require('./turrets.js');
+require('./bosses.js');
+
+// Terrestrials
+let rokna = new LayeredBoss(null, "Rokna", "terrestrial", 7, "aqua", "terrestrialTrapTurret", 7, 5.5);
+rokna.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["shotgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+rokna.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["pentashotTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);

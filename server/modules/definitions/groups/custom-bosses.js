cc94f22d8420fb4f5d5cec4c17c135ccb4ca1363
wplude114 <wplude@icloud.com>
2025-11-29 11:17:59 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -14,7 +14,7 @@ rokna.addLayer({turret: {
 }}, true, 6.5);
 rokna.addLayer({turret: {
     POSITION: [9.5, 7.5, 0, null, 160, 0],
-    TYPE: ["pentashotTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+    TYPE: ["boomerTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
 }}, true, 6.5);
 
 // Eternal

ce53966036ef4a54267339980db08af5c266036f
wplude114 <wplude@icloud.com>
2025-11-28 18:46:48 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -7,14 +7,14 @@ require('./turrets.js');
 require('./bosses.js');
 // Proto-Terrestrials
 // --- Gersemi
-let pgersemi = new LayeredBoss(null, "Proto-Gersemi", "Pterrestrial", 5, "lightGreen", "terrestrialTrapTurret", 5, 5.5);
-pgersemi.addLayer({turret: {
+let progersemi = new LayeredBoss(null, "Proto-Gersemi", "Pterrestrial", 5, "lightGreen", "terrestrialTrapTurret", 5, 5.5);
+progersemi.addLayer({turret: {
     POSITION: [9, 8, 0, null, 160, 0],
     TYPE: ["swarmTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.7, damage: 1.2} }],
 }});
 // --- Ares
-let pares = new LayeredBoss(null, "Proto-Ares", "Pterrestrial", 5, "purple", "terrestrialTrapTurret", 5, 5.5);
-pares.addLayer({gun: {
+let proares = new LayeredBoss(null, "Proto-Ares", "Pterrestrial", 5, "purple", "terrestrialTrapTurret", 5, 5.5);
+proares.addLayer({gun: {
     POSITION: [3.75, 7, 1.2, 8, 0, null, 0],
     PROPERTIES: {
         SHOOT_SETTINGS: combineStats([g.drone, g.summoner, g.destroyer, {health: 1.2, damage: 1.1, resist: 1.1, density: 1.5, maxSpeed: 1.25}]),
@@ -26,8 +26,8 @@ pares.addLayer({gun: {
     },
 }}, false, null, 18);
 // --- Rokna
-let prokna = new LayeredBoss(null, "Proto-Rokna", "Pterrestrial", 5, "aqua", "terrestrialTrapTurret", 5, 5.5);
-prokna.addLayer({turret: {
+let prorokna = new LayeredBoss(null, "Proto-Rokna", "Pterrestrial", 5, "aqua", "terrestrialTrapTurret", 5, 5.5);
+prorokna.addLayer({turret: {
     POSITION: [9.5, 7.5, 0, null, 160, 0],
     TYPE: ["shotgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
 }}, true, 6.5);

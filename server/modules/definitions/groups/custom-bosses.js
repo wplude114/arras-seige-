2cd1417af5a3dd21b2cb3f8914fc13e77039f910
wplude114 <wplude@icloud.com>
2025-11-29 12:38:49 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -6,6 +6,92 @@ require('./tanks.js');
 require('./turrets.js');
 require('./bosses.js');
 
+//shiny mysticals
+Class.shinysorcerer = {
+    PARENT: "miniboss",
+    LABEL: "Sorcerer",
+    DANGER: 8,
+    SHAPE: 0,
+    COLOR: "lightgreen",
+    UPGRADE_COLOR: "veryLightGrey",
+    SIZE: 28,
+    MAX_CHILDREN: 75,
+    VALUE: 5e5,
+    BODY: {
+        FOV: 0.7,
+        SPEED: 0.22 * base.SPEED,
+        HEALTH: 10 * base.HEALTH,
+        DAMAGE: 3 * base.DAMAGE,
+    },
+    GUNS: weaponArray({
+        POSITION: [3.5, 8.65, 1.2, 8, 0, 0, 0],
+        PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.drone, g.summoner, g.machineGun, g.machineGunner, { damage: 1.8, size: 0.5, spray: 150, speed: 2, shudder: 1.75, color: "lightgreen" }]),
+            TYPE: "minichip",
+            AUTOFIRE: true,
+            SYNCS_SKILLS: true,
+            STAT_CALCULATOR: "drone",
+            WAIT_TO_CYCLE: true,
+        },
+    }, 2)
+};
+Class.shinysummoner = {
+    PARENT: "miniboss",
+    LABEL: "Summoner",
+    DANGER: 8,
+    SHAPE: 4,
+    COLOR: "lightgreen",
+    UPGRADE_COLOR: "gold",
+    SIZE: 28,
+    MAX_CHILDREN: 35,
+    VALUE: 7e5,
+    BODY: {
+        FOV: 0.7,
+        SPEED: 0.2 * base.SPEED,
+        HEALTH: 12 * base.HEALTH,
+        DAMAGE: 4 * base.DAMAGE,
+    },
+    GUNS: weaponArray({
+        POSITION: [3.5, 8.65, 1.2, 8, 0, 0, 0],
+        PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.drone, g.summoner, { size: 0.8, color: "lightgreen" }]),
+            TYPE: "summonerDrone",
+            AUTOFIRE: true,
+            SYNCS_SKILLS: true,
+            STAT_CALCULATOR: "drone",
+            WAIT_TO_CYCLE: true,
+        },
+    }, 4)
+};
+Class.shinyenchantress = {
+    PARENT: "miniboss",
+    LABEL: "Enchantress",
+    DANGER: 8,
+    SHAPE: 3.5,
+    COLOR: "lightgreen",
+    UPGRADE_COLOR: "orange",
+    SIZE: 26,
+    MAX_CHILDREN: 28,
+    VALUE: 1e6,
+    BODY: {
+        FOV: 0.7,
+        SPEED: 0.15 * base.SPEED,
+        HEALTH: 17 * base.HEALTH,
+        DAMAGE: 7 * base.DAMAGE,
+    },
+    GUNS: weaponArray({
+        POSITION: [3.5, 8.65, 1.2, 8, 0, 0, 0],
+        PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.drone, g.summoner, { size: 1, damage: 1.1, color: "lightgreen" }]),
+            TYPE: "dorito",
+            AUTOFIRE: true,
+            SYNCS_SKILLS: true,
+            STAT_CALCULATOR: "drone",
+            WAIT_TO_CYCLE: true,
+        },
+    }, 3)
+};
+
 // Terrestrials
 let rokna = new LayeredBoss(null, "Rokna", "terrestrial", 7, "aqua", "terrestrialTrapTurret", 7, 5.5);
 rokna.addLayer({turret: {

12f9413e25736a0a3c18665368fdb6e26e59c554
wplude114 <wplude@icloud.com>
2025-11-29 14:04:15 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -102,3 +102,40 @@ rokna.addLayer({turret: {
     POSITION: [9.5, 7.5, 0, null, 160, 0],
     TYPE: ["boomerTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
 }}, true, 6.5);
+
+// Eternal
+let menace = new LayeredBoss(null, "Menace", "eternal", 13, "grey", "terrestrialTrapTurret", 13, 5.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["shotgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["pentashotTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["machineTripleTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["boomerTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+
+let zeus = new LayeredBoss(null, "Zeus", "eternal", 15, "mustard", "terrestrialTrapTurret", 15, 5.5);
+zeus.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["hyperTwisterTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 2.8, damage: 3.3} }],
+}}, true, 6.5);
+zeus.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["nailgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 2.8, damage: 3.3} }],
+}}, true, 6.5);
+zeus.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["carrierTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 2.8, damage: 3.3} }],
+}}, true, 6.5);
+zeus.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["shotgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 2.8, damage: 3.3} }],
+}}, true, 6.5);

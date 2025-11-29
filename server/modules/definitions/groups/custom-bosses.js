a2599d96a82031907a316cea39f8db53ec252538
wplude114 <wplude@icloud.com>
2025-11-28 20:30:09 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -16,3 +16,23 @@ rokna.addLayer({turret: {
     POSITION: [9.5, 7.5, 0, null, 160, 0],
     TYPE: ["pentashotTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
 }}, true, 6.5);
+
+// Eternal
+let menace = new LayeredBoss(null, "The Menace", "eternal", 13, "grey", "terrestrialTrapTurret", 13, 5.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["shotgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["bigAuto4Gun", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["machineTripleTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+menace.addLayer({turret: {
+    POSITION: [9.5, 7.5, 0, null, 160, 0],
+    TYPE: ["boomerTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
+}}, true, 6.5);
+

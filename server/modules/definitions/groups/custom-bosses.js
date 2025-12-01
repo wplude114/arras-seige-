5cb86c4f57eca681c04d32a84ba8ed513dc1eb27
wplude114 <wplude@icloud.com>
2025-12-01 17:23:48 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -171,7 +171,7 @@ rokna.addLayer({turret: {
     TYPE: ["boomerTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],
 }}, true, 6.5);
 
-let hera = new LayeredBoss("he", "Hera", "terrestrial", 7, "aqua", "terrestrialTrapTurret", 6.5, 5.5);
+let hera = new LayeredBoss("he", "Hera", "terrestrial", 7, 17, "terrestrialTrapTurret", 6.5, 5.5);
 hera.addLayer({turret: {
     POSITION: [9.5, 7.5, 0, null, 160, 0],
     TYPE: ["pentashotTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],

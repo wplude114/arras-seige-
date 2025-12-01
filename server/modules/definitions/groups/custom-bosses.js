22bf25b11dfbebdaa38fddb12b3b1f04146cfbef
wplude114 <wplude@icloud.com>
2025-11-30 20:01:18 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -113,7 +113,7 @@ Class.shinyenchantress = {
 };
 
 // Terrestrials
-let rokna = new LayeredBoss(null, "Hestia", "terrestrial", 7, "aqua", "terrestrialTrapTurret", 6.5, 5.5);
+let rokna = new LayeredBoss("rokna", "Hestia", "terrestrial", 7, "aqua", "terrestrialTrapTurret", 6.5, 5.5);
 rokna.addLayer({turret: {
     POSITION: [9.5, 7.5, 0, null, 160, 0],
     TYPE: ["shotgunTurret", { INDEPENDENT: true, GUN_STAT_SCALE: {health: 1.8, damage: 1.3} }],

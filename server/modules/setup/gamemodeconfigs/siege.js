3922dfec6f4e233f7b276d09b3d165c3ec3c771a
wplude114 <wplude@icloud.com>
2025-12-01 11:55:11 -0600

    Update siege.js

--- server/modules/setup/gamemodeconfigs/siege.js
+++ server/modules/setup/gamemodeconfigs/siege.js
@@ -5,8 +5,8 @@ module.exports = {
     BOSS_SPAWN_COOLDOWN: Number.MAX_SAFE_INTEGER,
     WAVES: 250, // CLASSIC_SIEGE: false to use this setting
     CLASSIC_SIEGE: false,
-    TILE_WIDTH: 100,
-    TILE_HEIGHT: 100,
+    TILE_WIDTH: 200,
+    TILE_HEIGHT: 200,
     ROOM_SETUP: ['map_siege_legacy'],
     //BOT_XP: 500,
 };

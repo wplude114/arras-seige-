aa75042f9c1969c03c164d8e924547bf7bd0ad6e
wplude114 <wplude@icloud.com>
2025-12-01 23:48:57 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -126,10 +126,10 @@ zeus.addLayer({turret: {
 
 let gaia = new LayeredBoss("ga", "Gaia", "entrestrial", 5, "lightGreen", "terrestrialTrapTurret", 4, 5);
 gaia.addLayer({gun: {
-    POSITION: [2.25, 10, -1.6, 9, 0, null, 0],
+    POSITION: [2, 10, -1.6, 8.85, 0, null, 0],
     PROPERTIES: {
         SHOOT_SETTINGS: combineStats([g.drone, g.summoner, g.pounder, g.destroyer, {speed: 6.25, maxSpeed: 8}]),
-        TYPE: ["realchip", {INDEPENDENT: true, DRAW_HEALTH: true, COLOR: 'lightGreen', shape: 3}],
+        TYPE: ["realchip", {INDEPENDENT: true, DRAW_HEALTH: true, COLOR: 'lightGreen', SHAPE: 5}],
         AUTOFIRE: true,
         SYNCS_SKILLS: true,
     },

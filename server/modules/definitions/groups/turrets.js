141ddaf30ab436831c40a00f5b65802e337f0669
wplude114 <wplude@icloud.com>
2025-12-02 16:04:11 -0600

    Update turrets.js

--- server/modules/definitions/groups/turrets.js
+++ server/modules/definitions/groups/turrets.js
@@ -249,6 +249,53 @@ Class.hyperTwisterTurret = makeTurret({
 }, {canRepel: true, limitFov: true, color: 'mirror', extraStats: []})
 Class.rocketeerTurret = makeTurret('rocketeer', {canRepel: true, limitFov: true})
 Class.boomerTurret = makeTurret('boomer', {canRepel: true, limitFov: true, color: 'mirror', extraStats: []})
+Class.minigunNesterTurret = makeTurret({
+    GUNS: [
+        {
+            POSITION: [16, 7.5, 1, 0, 4.5, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        }, {
+            POSITION: [11.5, 7.5, -1.33, 1, 4.5, 0, 0.5],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        }, {
+            POSITION: [16, 7.5, 1, 0, -4.5, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        }, {
+            POSITION: [11.5, 7.5, -1.33, 1, -4.5, 0, 0.5],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        }, {
+            POSITION: [22.5, 9, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        }, {
+            POSITION: [20.4, 9, 1, 0, 0, 0, 1/3],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        }, {
+            POSITION: [18.3, 9, 1, 0, 0, 0, 2/3],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minigun, g.twin, g.spam, g.spam]),
+                TYPE: "bullet",
+            },
+        },
+    ],
+}, {canRepel: true, limitFov: true, color: 'mirror', extraStats: []})
 Class.triTrapGuardTurret = {
     PARENT: "genericTank",
     COLOR: -1,

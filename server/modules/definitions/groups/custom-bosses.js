599b81e2bbcabad749d8cee133b641b5307d529e
wplude114 <wplude@icloud.com>
2025-12-03 12:52:21 -0600

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -133,7 +133,7 @@ Class.alphaExorcistor = {
             TYPE: [ "pentdeco", { INDEPENDENT: true, COLOR: -1} ],
         },
         {
-            POSITION: [8, 0, 0, 0, 0, 1],
+            POSITION: [11, 0, 0, 0, 0, 1],
             TYPE: [ "pentdeco2", { INDEPENDENT: true, COLOR: -1} ],
         },
     ],

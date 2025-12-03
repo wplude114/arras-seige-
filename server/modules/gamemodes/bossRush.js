24b7b8b3af7ec2f6a0e15d6dd448aedf24fabda2
wplude114 <wplude@icloud.com>
2025-12-03 13:10:09 -0600

    Update bossRush.js

--- server/modules/gamemodes/bossRush.js
+++ server/modules/gamemodes/bossRush.js
@@ -71,6 +71,7 @@ class BossRush {
             [  3, "eliteBattleship"],
             [  4, "eliteSpinner"],
             [  4, "eliteSprayer"],
+            [200, "legionaryCrasher"],
         ];
         this.b_nest = [
             [  4, "nestWatcher"],
@@ -109,10 +110,23 @@ class BossRush {
             [100, "kronos"],
             [100, "odin"],
         ];
+        this.b_shiny= [
+            [  1, "shinySentryGun"],
+            [  1, "shinySentrySwarm"],
+            [  1, "shinySentryTrap"],
+            [ 10, "ga"],
+            [ 15, "gersemi"],
+            [ 35, "freyja"],
+        ];
         this.b_sentry = [
             [  0.5, "sentryGun"],
             [  0.5, "sentrySwarm"],
             [  0.5, "sentryTrap"],
+        ];
+        this.b_shinySentry = [
+            [  1, "shinySentryGun"],
+            [  1, "shinySentrySwarm"],
+            [  1, "shinySentryTrap"],
         ];
         this.b_crasher = [
             [  0.5, "crasher"],
@@ -142,13 +156,16 @@ class BossRush {
                 points = calculatePoints(i)
                 let choices = this.b_normal
                 let r = Math.random()
-                if(r<0.01 && i>39){choices = this.b_eternal;} //only eternals, idk odds lol
+                if(r<0.10 && i>39){choices = this.b_eternal;} //only eternals, idk odds lol
                 if(r<0.025 && i>24){choices = this.b_celestial;} //celestial wave
                 else if(r<0.05 && i>1){choices = this.b_crasher;} //crasher wave
                 else if(r<0.075 && i>4){choices = this.b_sentry;} //sentry wave
                 else if(r<0.1 && i>9){choices = this.b_nest;} //nest wave
                 else if(r<0.15 && i>9){choices = this.b_elite;} //elite wave
                 else if(r<0.25 && i>4){choices = this.b_mystical;} //mystical wave
+                    //rareest waves
+                else if(r<0.95 && i>9){choices = this.b_shinySentry;} // shiny sentry wave
+                else if(r<0.99 && i>14){choices = this.b_shiny;} // rarest wave, all shiny color things
                 else{choices = this.b_normal}
 
             while (points > 0 && choices.length) {

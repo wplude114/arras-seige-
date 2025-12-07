7d3e850b718b8f06c5e2f581b6418c8f6c57bfa3
wplude114 <wplude@icloud.com>
2025-12-06 23:56:25 -0600

    Update old_dreadnoughts.js

--- server/modules/setup/gamemodeconfigs/old_dreadnoughts.js
+++ server/modules/setup/gamemodeconfigs/old_dreadnoughts.js
@@ -7,5 +7,20 @@ module.exports = {
     TILE_HEIGHT: 600,
     PORTAL_SPAWNS: true,
     MAX_UPGRADE_TIER: 13,
-    SPAWN_CONFINEMENT: {xMin: 18000}
+    SPAWN_CONFINEMENT: {xMin: 18000},
+    FOOD_TYPES: [
+        [9999, [
+            [1024, 'egg'], [256, 'betaEgg'], [64, 'alphaEgg'],
+            [256, 'square'], [64, 'betaSquare'], [16, 'alphaSquare'],
+            [64, 'triangle'],
+            [16, 'pentagon'], [4, 'GBetaPentagon'], [1, 'GAlphaPentagon'], //[0.25, 'omegaPentagon']
+        ]],
+    ],
+    FOOD_TYPES_NEST: [
+        [1, [
+            [16, 'pentagon'], [ 4, 'GBetaPentagon'], [ 1, 'GAlphaPentagon'],
+            [0.5, 'GSeptagon'], [0.25, 'GHexagon'], [ 0.05, 'GOctagon'], [ 0.0075, 'GNonagon']
+        ]],
+        */
+    ],
 };

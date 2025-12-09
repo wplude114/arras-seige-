b72f16749acf56b420724326a942545ab252d4c5
wplude114 <wplude@icloud.com>
2025-12-09 09:27:27 -0600

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2022,8 +2022,8 @@ function drawLeaderboard(spacing, alcoveSize, max) {
 function drawAvailableUpgrades(spacing, alcoveSize) {
     // Draw upgrade menu
     if (gui.upgrades.length > 0) {
-        let internalSpacing = 7;
-        let len = alcoveSize / 2;
+        let internalSpacing = 5;
+        let len = alcoveSize / 5;
         let height = len;
 
         // Animation processing

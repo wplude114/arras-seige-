83bc571e0548eff2883c14ff13fa9a4a32fb5063
wplude114 <wplude@icloud.com>
2025-12-09 14:31:57 -0600

    Update tanks.js

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -28,7 +28,46 @@ Class.basic = {
         },
     ]
 }
+Class.trapLayer = {
+    PARENT: "genericTank",
+    LABEL: "Trap-Layer",
+    DANGER: 5,
+    GUNS: [
+        {
+            POSITION: {LENGTH: 17,WIDTH: 10,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.fake]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 20,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.fake]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 5,WIDTH: 8,ASPECT: 1, X: 14,Y: 0,ANGLE: 0,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.fake]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 3,WIDTH: 8,ASPECT: 1.4, X: 16,Y: 0,ANGLE: 0,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic]),
+                TYPE: "trap",
+            }
+        },
+        {
+            POSITION: {LENGTH: 12,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+        },
+    ]
+}
 
 // Upgrade Paths
-//Class.basic.UPGRADES_TIER_1 = []
+Class.basic.UPGRADES_TIER_1 = ['trapLayer']
+
 

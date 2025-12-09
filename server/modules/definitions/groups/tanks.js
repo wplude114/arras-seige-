5e50b34eb718ed57b8b067068d8381493b62fd6f
wplude114 <wplude@icloud.com>
2025-12-09 14:22:28 -0600

    Update tanks.js

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -10,24 +10,25 @@ Class.basic = {
     DANGER: 4,
     GUNS: [
         {
-            POSITION: {LENGTH: 18,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+            POSITION: {LENGTH: 20,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic, g.fake]),
                 TYPE: "bullet",
             }
         },
         {
-            POSITION: {LENGTH: 5,WIDTH: 8,ASPECT: 1, X: 12,Y: 0,ANGLE: 0,DELAY: 0},
+            POSITION: {LENGTH: 5,WIDTH: 8,ASPECT: 1, X: 14,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
                 SHOOT_SETTINGS: combineStats([g.basic]),
                 TYPE: "bullet",
             }
         },
         {
-            POSITION: {LENGTH: 10,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+            POSITION: {LENGTH: 12,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
         },
     ]
 }
 
 // Upgrade Paths
-//Class.basic.UPGRADES_TIER_1 = ["twin", "sniper", "machineGun", "flankGuard", "director", "pounder", "trapper"]
+//Class.basic.UPGRADES_TIER_1 = []
+

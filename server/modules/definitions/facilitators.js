5ab44be0a8ae02ee78c4f3d045b99a7c37a63a54
wplude114 <wplude@icloud.com>
2025-12-09 02:55:38 -0600

    Update facilitators.js

--- server/modules/definitions/facilitators.js
+++ server/modules/definitions/facilitators.js
@@ -641,13 +641,11 @@ exports.menuvar2 = (name = -1, color = -1, shape = 0) => {
             }
         },
         {
-            POSITION: [15, 10, 0.5, 0, 0, 0, 0],
-        },
-        {
-            POSITION: [8, 12, -0.00001, 12, 0, 0, 0],
+            POSITION: [20, 10, -0.0001, 0, 0, 0, 0],
             PROPERTIES: {
                 SHOOT_SETTINGS: exports.combineStats([g.basic, g.fake]),
-                TYPE: "bullet"
+                TYPE: "bullet",
+				COLOR: "black"
             }
         },
     ];

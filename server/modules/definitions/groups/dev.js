5141bd51bac4a68157e834951c14a32497987beb
wplude114 <wplude@icloud.com>
2025-12-08 20:18:16 -0600

    Update dev.js

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -359,7 +359,10 @@ Class.barrelDecoTest = {
         },
         {
             POSITION: {LENGTH: 13,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
-        }
+        },
+        {
+            POSITION: {LENGTH: 8,WIDTH: 10,ASPECT: 0.8, X: 9.5,Y: 0,ANGLE: 180,DELAY: 0},
+        },
     ]
 }
 

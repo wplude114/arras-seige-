824e34dfeba26e6347908b2a6d885b1ad39cac97
wplude114 <wplude@icloud.com>
2026-06-12 05:36:00 -0500

    Update custom-food.js

--- server/modules/definitions/groups/custom-food.js
+++ server/modules/definitions/groups/custom-food.js
@@ -6,14 +6,15 @@ Class.hexas = {
     LABEL: "Hexas",
     VALUE: 5000,
     SIZE: 24,
+    CONTROLLERS: ["spin","nearestDifferentMaster", ["minion", {orbit: 80}]],
     BODY: {FOV: 0.8},
     GUNS: [],
     TURRETS: [
         {
-            POSITION: [7, 10, 0, 60, 180, 0],
+            POSITION: [7, 10, 0, 60, 160, 0],
             TYPE: "autoTankGun",
         }, {
-            POSITION: [7, 10, 0, -120, 180, 0],
+            POSITION: [7, 10, 0, -120, 160, 0],
             TYPE: "autoTankGun",
         },
     ],

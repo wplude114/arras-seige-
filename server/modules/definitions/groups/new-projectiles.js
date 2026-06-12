74c6040f5490338b53b8ee7c1264cddca9fb53f9
wplude114 <wplude@icloud.com>
2026-06-12 17:25:24 -0500

    Update new-projectiles.js

--- server/modules/definitions/groups/new-projectiles.js
+++ server/modules/definitions/groups/new-projectiles.js
@@ -7,8 +7,50 @@ Class.flailBullet = {
     COLOR: "grey",
     HITS_OWN_TYPE: 'hard',
     INDEPENDENT: true,
+    FACING_TYPE: ['spin', {speed: 0.05}],
     TURRETS: [{
         POSITION: [21.5, 0, 0, 0, 360, 0],
         TYPE: "flailBallSpike",
     }],
+}
+
+Class.flailSplitBullet = {
+    PARENT: "bullet",
+    COLOR: "grey",
+    INDEPENDENT: true,
+    FACING_TYPE: ['spin', {speed: 0.05}],
+    TURRETS: [{
+        POSITION: [21.5, 0, 0, 0, 360, 0],
+        TYPE: "flailBallSpike",
+    },{
+        POSITION: [5, 0, 0, 0, 0, 1],
+        TYPE: "genericEntity",
+    }],
+
+    GUNS: [
+        {
+            POSITION: [8, 8, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic]),
+                TYPE: ["flailBullet", { PERSISTS_AFTER_DEATH: true }],
+                SHOOT_ON_DEATH: true,
+            }
+        },
+        {
+            POSITION: [8, 8, 1, 0, 0, 120, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic]),
+                TYPE: ["flailBullet", { PERSISTS_AFTER_DEATH: true }],
+                SHOOT_ON_DEATH: true,
+            }
+        },
+        {
+            POSITION: [8, 8, 1, 0, 0, -120, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic]),
+                TYPE: ["flailBullet", { PERSISTS_AFTER_DEATH: true }],
+                SHOOT_ON_DEATH: true,
+            }
+        },
+    ]
 }

1a244f8b1719d47bb6d03c84f0fd14e3c83f2596
wplude114 <wplude@icloud.com>
2026-06-11 21:21:42 -0500

    Update custom-bosses.js

--- server/modules/definitions/groups/custom-bosses.js
+++ server/modules/definitions/groups/custom-bosses.js
@@ -279,7 +279,7 @@ Class.nestWatcher = {
 // Telestials
 let Titon = new LayeredBoss("Ti", "Titon", "entrestrial", 5, "veryLightGrey", "terrestrialTrapTurret", 4, 5);
 Titon.addLayer({turret: {
-    POSITION: [7, 8, 0, null, 120, 0],
+    POSITION: [6, 8, 0, null, 120, 0],
     TYPE: ["flailBolt2", { INDEPENDENT: true, COLOR: -1 } ],
 }}, true, 10, 10);
 

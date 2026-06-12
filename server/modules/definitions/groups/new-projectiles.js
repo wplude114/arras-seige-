f4d362723ce51c9a7190c185294b687fe8850a02
wplude114 <wplude@icloud.com>
2026-06-12 17:04:44 -0500

    Update new-projectiles.js

--- server/modules/definitions/groups/new-projectiles.js
+++ server/modules/definitions/groups/new-projectiles.js
@@ -1,1 +1,14 @@
-// Note: put stuff here.
+const { combineStats, makeAuto, weaponArray } = require('../facilitators.js');
+const { base } = require('../constants.js');
+const g = require('../gunvals.js');
+
+Class.flailBullet = {
+    PARENT: "genericTank",
+    COLOR: "grey",
+    HITS_OWN_TYPE: 'hard',
+    INDEPENDENT: true,
+    TURRETS: [{
+        POSITION: [21.5, 0, 0, 0, 360, 0],
+        TYPE: "flailBallSpike",
+    }],
+}

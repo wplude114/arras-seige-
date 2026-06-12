9320f0ab8cee9297b20f0d708523855288bdcb34
wplude114 <wplude@icloud.com>
2026-06-12 06:14:30 -0500

    Update custom-food.js

--- server/modules/definitions/groups/custom-food.js
+++ server/modules/definitions/groups/custom-food.js
@@ -1,16 +1,42 @@
 const { basePolygonDamage, basePolygonHealth } = require('../constants.js');
 const { makeRelic, makeRare, makePrimordial, makeCrasher, makeLaby } = require('../facilitators.js');
 
-Class.hexas = {
+Class.protoDread = {
     PARENT: "genericTank",
     LABEL: "Proto-Dread",
     SIZE: 24,
     SHAPE: 6,
     COLOR: 17,
     FACING_TYPE: ['spin', {speed: 0.02}],
-    BODY: {FOV: 0.8},
-    GUNS: [],
+    BODY: {FOV: 1.1},
+};
+
+Class.protoDread1 = {
+    PARENT: "protoDread",
+    LABEL: "Proto-Dread",
     TURRETS: [
+        {
+            POSITION: [7, 10, 0, 0, 160, 0],
+            TYPE: "autoTankGun",
+        }, {
+            POSITION: [7, 10, 0, 180, 160, 0],
+            TYPE: "autoTankGun",
+        },
+    ],
+};
+
+Class.protoJuggernaut = {
+    PARENT: "protoDread",
+    LABEL: "Proto-Juggernaut",
+    TURRETS: [
+        {
+            POSITION: [7, 10, 0, 0, 160, 0],
+            TYPE: "autoTankGun",
+        }, {
+            POSITION: [7, 10, 0, 180, 160, 0],
+            TYPE: "autoTankGun",
+        },
+        
         {
             POSITION: [7, 10, 0, 60, 160, 0],
             TYPE: "autoTankGun",

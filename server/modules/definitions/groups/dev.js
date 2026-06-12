626aa8d51c71cefd8a1c42dd660562558cd3c544
wplude114 <wplude@icloud.com>
2026-06-12 17:43:21 -0500

    Update dev.js

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -2377,14 +2377,14 @@ Class.lobber = {
         {
             POSITION: {LENGTH: 20,WIDTH: 16,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.lobber, g.fake]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber, g.fake]),
                 TYPE: "bullet",
             }
         },
         {
             POSITION: {LENGTH: 5,WIDTH: 18,ASPECT: 1, X: 14,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.lobber]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber]),
                 TYPE: "flailBullet",
             }
         },
@@ -2401,14 +2401,14 @@ Class.shatterShot = {
         {
             POSITION: {LENGTH: 20,WIDTH: 16,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.lobber, g.pounder, g.fake]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber, g.pounder, g.fake]),
                 TYPE: "bullet",
             }
         },
         {
             POSITION: {LENGTH: 5,WIDTH: 18,ASPECT: 1, X: 14,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.lobber, g.pounder]),
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber, g.pounder]),
                 TYPE: "flailSplitBullet",
             }
         },

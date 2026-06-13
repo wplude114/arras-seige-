b33cc1e3e7cc0968f1c6873d4a8d415d46350a98
wplude114 <wplude@icloud.com>
2026-06-13 18:52:07 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -244,19 +244,19 @@ module.exports = {
     BOSS_TYPES: [
     { // elites I
         bosses: ["eliteDestroyer", "eliteGunner", "eliteBattleship", "eliteSpawner", "eliteTrapGuard",],
-        amount: [1], chance: 20,
+        amount: [5,1], chance: 20,
     },
     { // sentinels
         bosses: ["sentinelMinigun", "sentinelLauncher", "sentinelCrossbow"],
-        amount: [7], chance: 15,
+        amount: [20,5], chance: 15,
     },
     { // telestials I
         bosses: ["th", "ze", "ga", "ha", "de", "the"],
-        amount: [2], chance: 15,
+        amount: [2,1], chance: 15,
     },
     { // telestials II
         bosses: ["th", "ze", "ga", "ha"],
-        amount: [4], chance: 10,
+        amount: [5,3], chance: 10,
     },],
 
 

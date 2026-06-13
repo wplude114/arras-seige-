b14f2c50c138a8bb73b25cd36c98708708f845c8
wplude114 <wplude@icloud.com>
2026-06-13 16:47:19 -0500

    Update Lancer.js

--- server/modules/definitions/groups/Lancer.js
+++ server/modules/definitions/groups/Lancer.js
@@ -2,7 +2,7 @@ const { combineStats, makeAuto, makeOver, makeDeco, makeGuard, makeBird, makeRad
 const { base, statnames, dfltskl, smshskl } = require('../constants.js');
 require('./generics.js');
 const g = require('../gunvals.js');
-
+// tracker3gun
 Class.lancer = {
   PARENT: "genericTank",
   LABEL: "Lancer",
@@ -32,7 +32,47 @@ Class.lancer = {
   ]
 }
 
+Class.trackerLancer = {
+  PARENT: "genericTank",
+  LABEL: "Lancer",
+  BODY: {
+    SPEED: base.SPEED * 1.2,
+    DAMAGE: base.DAMAGE * 0.9
+  },
+  STAT_NAMES: {
+    BULLET_SPEED: 'Lance Range',
+    BULLET_HEALTH: 'Lance Longevity',
+    BULLET_PEN: 'Lance Sharpness',
+    BULLET_DAMAGE: 'Lance Damage',
+    RELOAD: 'Lance Density'
+  },
+  GUNS: [
+    {
+      POSITION: [20, 15, 0.001, 0, 0, 0, 0],
+      PROPERTIES: {
+        SHOOT_SETTINGS: combineStats([g.basic, { reload: 0.4, speed: 0.1, maxSpeed: 0.1, range: 0.1, recoil: 0}]),
+        TYPE: ["bullet", { ALPHA: 0 }],
+        AUTOFIRE: true
+      }
+    },
+    {
+      POSITION: [25, 15, 0.001, 0, 0, 0, 0]
+    }
+  ],
+  TURRETS: [{
+        POSITION: [10, 0, 0, 180, 360, 1],
+        TYPE: [
+            "tracker3Gun",
+            {
+                CONTROLLERS: ["nearestDifferentMaster"],
+                INDEPENDENT: true,
+                COLOR: 16,
+            },
+        ],
+    }]
+}
+
 Class.hawk = makeBird("lancer", "Hawk")
 Class.wasp = makeOver("lancer", "Wasp", {count: 1, independent: true, cycle: true})
 
-Class.lancer.UPGRADES_TIER_3 = ["hawk", "wasp"]
+Class.lancer.UPGRADES_TIER_3 = ["hawk", "wasp", "trackerLancer"]

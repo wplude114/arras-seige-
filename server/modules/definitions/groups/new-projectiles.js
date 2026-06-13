30ca6c3de67451a9df904c9cb3ec1066b7387cfb
wplude114 <wplude@icloud.com>
2026-06-12 19:55:51 -0500

    Update new-projectiles.js

--- server/modules/definitions/groups/new-projectiles.js
+++ server/modules/definitions/groups/new-projectiles.js
@@ -2,6 +2,20 @@ const { combineStats, makeAuto, weaponArray } = require('../facilitators.js');
 const { base } = require('../constants.js');
 const g = require('../gunvals.js');
 
+// bullet turrets
+Class.autoMissileGun = makeTurret({
+    GUNS: [
+        {
+            POSITION: [22, 11, 1, 0, 0, 180, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.minionGun, g.turret, g.power, g.autoTurret, { density: 0.1 }]),
+                TYPE: "bullet",
+                WAIT_TO_CYCLE: true
+            },
+        },
+    ],
+}, {independent: true, extraStats: []})
+
 Class.flailBullet = {
     PARENT: "bullet",
     HITS_OWN_TYPE: 'hard',
@@ -75,4 +89,18 @@ Class.flailSplitBullet = {
             }
         },
     ]
+}
+
+Class.flailMissle = {
+    PARENT: "bullet",
+    HITS_OWN_TYPE: 'hard',
+    INDEPENDENT: true,
+    FACING_TYPE: ['spin', {speed: 0.05}],
+    TURRETS: [{
+        POSITION: [21.5, 0, 0, 0, 360, 0],
+        TYPE: "flailBallSpike",
+    },{
+        POSITION: [7, 0, 0, 0, 0, 1],
+        TYPE: "autoMissleGun",
+    }],
 }

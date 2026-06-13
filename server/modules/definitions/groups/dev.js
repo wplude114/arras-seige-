f322cf393a065ca017688be75f6333436e7f7b1b
wplude114 <wplude@icloud.com>
2026-06-12 23:44:20 -0500

    Update dev.js

--- server/modules/definitions/groups/dev.js
+++ server/modules/definitions/groups/dev.js
@@ -2846,10 +2846,10 @@ Class.flace = {
     SKILL_CAP: [dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl, dfltskl],
 }
 
-Class.developer.UPGRADES_TIER_0 = ["nav", "misc"]
+Class.developer.UPGRADES_TIER_0 = ["nav"]
 Class.misc.UPGRADES_TIER_0 = ["eggGenerator", "spectator", "addons"]
-Class.nav.UPGRADES_TIER_0 = ["tanks", "bosses", "levels", "teams", "testing"] // makes things easier
-    Class.tanks.UPGRADES_TIER_0 = ["basic", "protoDread", "unavailable", "arenaCloser", "dominators", "sanctuaries", "mothership", "baseProtector", "antiTankMachineGun"]
+Class.nav.UPGRADES_TIER_0 = ["tanks", "bosses"] // makes things easier
+    Class.tanks.UPGRADES_TIER_0 = ["basic", "protoDread", "unavailable". ["whirlwind", "basic"], ["whirlwind", "flail"]]
         Class.unavailable.UPGRADES_TIER_0 = ["flail", "whirlwind"]
             Class.flail.UPGRADES_TIER_2 = ["doubleFlail", "mace", "flangle","lobber"]
                 Class.doubleFlail.UPGRADES_TIER_3 = ["tripleFlail"]
@@ -2863,7 +2863,7 @@ Class.nav.UPGRADES_TIER_0 = ["tanks", "bosses", "levels", "teams", "testing"] //
         Class.dominators.UPGRADES_TIER_0 = ["destroyerDominator", "gunnerDominator", "trapperDominator"]
         Class.sanctuaries.UPGRADES_TIER_0 = ["sanctuaryTier1", "sanctuaryTier2", "sanctuaryTier3", "sanctuaryTier4", "sanctuaryTier5", "sanctuaryTier6"]
 
-    Class.bosses.UPGRADES_TIER_0 = ["sentries", "elites", "mysticals", "nesters", "rogues", "rammers", "entrestrials", "terrestrials", "celestials", "eternals", "devBosses"]
+    Class.bosses.UPGRADES_TIER_0 = ["sentries", "elites", "mysticals", "nesters", "rogues", "rammers", "entrestrials", "terrestrials", "celestials", "eternals"]
         Class.sentries.UPGRADES_TIER_0 = ["sentrySwarm", "sentryGun", "sentryTrap", "shinySentrySwarm", "shinySentryGun", "shinySentryTrap", "sentinelMinigun", "sentinelLauncher", "sentinelCrossbow"]
         Class.elites.UPGRADES_TIER_0 = ["eliteDestroyer", "eliteGunner", "eliteSprayer", "eliteBattleship", "eliteSpawner", "eliteTrapGuard", "eliteSpinner", "eliteSkimmer", "legionaryCrasher", "guardian", "defender", "sprayerLegion"]
         Class.mysticals.UPGRADES_TIER_0 = ["sorcerer", "betaSorcerer", "summoner", "enchantress", "exorcistor", "shaman", 'betaExorcistor', 'alphaExorcistor', 'omegaExorcistor']

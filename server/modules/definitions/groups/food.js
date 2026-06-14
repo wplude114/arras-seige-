49d1aa1aebda441df6339b32cdff094f07caa145
wplude114 <wplude@icloud.com>
2026-06-14 00:20:09 -0500

    Update food.js

--- server/modules/definitions/groups/food.js
+++ server/modules/definitions/groups/food.js
@@ -94,6 +94,29 @@ Class.gem = {
     INTANGIBLE: false,
     GIVE_KILL_MESSAGE: true,
 };
+Class.computerizedGem = {
+    PARENT: "food",
+    LABEL: "Computerized Gem",
+    SIZE: 10,
+    SHAPE: 6,
+    COLOR: 17,
+    FACING_TYPE: ['spin', {speed: 0.02}],
+    BODY: {HEALTH: 50, DAMAGE: 3, FOV: 1.1},
+	VALUE: 63319,
+    TURRETS: [
+        {
+            POSITION: [7, 10, 0, 0, 160, 0],
+            TYPE: "autoTankGun",
+        }, {
+            POSITION: [7, 10, 0, 180, 160, 0],
+            TYPE: "autoTankGun",
+        },
+        {
+            POSITION: [10, 0, 0, 0, 0, 1],
+            TYPE: "gem",
+        },
+    ],
+}
 Class.jewel = {
     PARENT: "food",
     LABEL: "Jewel",

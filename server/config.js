9efd3707c5f0c7cb637c2076cc15d2189bdd7bae
wplude114 <wplude@icloud.com>
2026-06-14 00:34:37 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -244,7 +244,7 @@ module.exports = {
     // "th", "ze", "ga", "ha", "de", "the","ares", "gersemi", "ezekiel", "eris", "selene","rokna","paladin", "freyja", "zaphkiel", "nyx", "theia"
     BOSS_TYPES: [
         { // elite
-            bosses: ["gem","jewel","hexagon","computerizedGem","Ghexagon","the","rokna"],
+            bosses: ["gem","jewel","hexagon","computerizedGem","GHexagon","the","rokna"],
             amount: 10, chance: 1,
         },
         /*

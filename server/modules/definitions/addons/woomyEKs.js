e8a6881810f82b7ac5feeb772b07758e0dc07b26
wplude114 <wplude@icloud.com>
2026-06-15 22:13:32 -0500

    Add EKMenu and half_reload, half_recoil settings

--- server/modules/definitions/addons/woomyEKs.js
+++ server/modules/definitions/addons/woomyEKs.js
@@ -4,6 +4,7 @@ const g = require('../gunvals.js');
 const {makeTurret} = require("../facilitators");
 
 Class.EKMenu = menu("EK Menu", "veryLightGrey", 0)
+Class.EKMenu.REROOT_UPGRADE_TREE = "EKMenu"
 Class.EKMenu.UPGRADES_TIER_0 = []
 
 g.half_reload = {reload: 2}

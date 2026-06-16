318eff4475b23cb1a7848beeeb99110ccbf9652f
wplude114 <wplude@icloud.com>
2026-06-15 22:28:01 -0500

    Update woomyEKs.js

--- server/modules/definitions/addons/woomyEKs.js
+++ server/modules/definitions/addons/woomyEKs.js
@@ -5,7 +5,6 @@ const {makeTurret} = require("../facilitators");
 
 Class.EKMenu = menu("EK Menu", "veryLightGrey", 0)
 Class.EKMenu.UPGRADES_TIER_0 = []
-Class.EKMenu.REROOT_UPGRADE_TREE = "EKMenu"
 
 g.half_reload = {reload: 2}
 g.half_recoil = {recoil: 0.5}

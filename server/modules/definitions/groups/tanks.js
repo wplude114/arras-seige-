ce20db04e22e6cb2e03675cb37fe856c184098eb
wplude114 <wplude@icloud.com>
2026-06-22 05:14:50 -0500

    Add tri tank and update barrel upgrades

--- server/modules/definitions/groups/tanks.js
+++ server/modules/definitions/groups/tanks.js
@@ -31,7 +31,7 @@ Class.basic = {
 
 Class.barrel = {
     PARENT: "genericTank",
-    LABEL: "Barrel",
+    LABEL: "Basic",
     DANGER: 4,
     GUNS: [
        {
@@ -53,5 +53,69 @@ Class.barrel = {
         },
     ]
 }
+Class.tri = {
+    PARENT: "genericTank",
+    LABEL: "Tri",
+    DANGER: 5,
+    FACING_TYPE: ["spin", { speed: -0.05, independent: true }],
+    GUNS: [
+       {
+            POSITION: {LENGTH: 17,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber, g.fake]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 5,WIDTH: 8,ASPECT: 1, X: 14,Y: 0,ANGLE: 0,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 12,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
+        },
+
+        
+       {
+            POSITION: {LENGTH: 17,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: 120,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber, g.fake]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 5,WIDTH: 8,ASPECT: 1, X: 14,Y: 0,ANGLE: 120,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 12,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: 120,DELAY: 0},
+        },
+
+        
+       {
+            POSITION: {LENGTH: 17,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: -120,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber, g.fake]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 5,WIDTH: 8,ASPECT: 1, X: 14,Y: 0,ANGLE: -120,DELAY: 0},
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.lobber]),
+                TYPE: "bullet",
+            }
+        },
+        {
+            POSITION: {LENGTH: 12,WIDTH: 8,ASPECT: 1, X: 0,Y: 0,ANGLE: -120,DELAY: 0},
+        },
+    ]
+}
 
 Class.basic.UPGRADES_TIER_1 = ["barrel"]
+Class.barrel.UPGRADES_TIER_2 = ["tri"]

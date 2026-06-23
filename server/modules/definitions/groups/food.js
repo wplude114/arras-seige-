3341e0b637136d9ca44cea653f193d6f94fc9d57
wplude114 <wplude@icloud.com>
2026-06-23 09:03:46 -0500

    Add Beta Triangle and Octagon definitions to food.js

--- server/modules/definitions/groups/food.js
+++ server/modules/definitions/groups/food.js
@@ -18,6 +18,26 @@ Class.triangle = {
     DRAW_HEALTH: true,
     INTANGIBLE: false,
 };
+Class.betaTriangleTEST = {
+    PARENT: "food",
+    LABEL: "Beta Triangle",
+    VALUE: 300,
+    SHAPE: 3,
+    SIZE: 20,
+    COLOR: 5,
+    BODY: {
+        DAMAGE: basePolygonDamage,
+        DENSITY: 4,
+        HEALTH: basePolygonHealth*2,
+        PENETRATION: 2,
+        ACCELERATION: 0.0075
+    },
+    DRAW_HEALTH: true,
+    INTANGIBLE: false,
+    PROPS: [
+        {POSITION: [10,0,0,45,0,1]}
+    ]
+};
 
 Class.square = {
     PARENT: "food",
@@ -82,11 +102,30 @@ Class.septagon = {
     SIZE: 27,
     COLOR: 1,
     BODY: {
-        DAMAGE: 3 * basePolygonDamage,
-        DENSITY: 8,
-        HEALTH: 20 * basePolygonHealth,
-        RESIST: 1.3,
-        SHIELD: 50 * basePolygonHealth,
+        DAMAGE: 5 * basePolygonDamage,
+        DENSITY: 10,
+        HEALTH: 32 * basePolygonHealth,
+        RESIST: 1.4,
+        SHIELD: 64 * basePolygonHealth,
+        PENETRATION: 1.1,
+        ACCELERATION: 0.003
+    },
+    DRAW_HEALTH: true,
+};
+
+Class.octagon = {
+    PARENT: "food",
+    LABEL: "Octagon",
+    VALUE: 1330,
+    SHAPE: 8,
+    SIZE: 36,
+    COLOR: "lavender",
+    BODY: {
+        DAMAGE: 6 * basePolygonDamage,
+        DENSITY: 20,
+        HEALTH: 52 * basePolygonHealth,
+        RESIST: 1.6,
+        SHIELD: 76 * basePolygonHealth,
         PENETRATION: 1.1,
         ACCELERATION: 0.003
     },

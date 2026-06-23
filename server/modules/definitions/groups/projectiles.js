78d75b9071661d0f7ecd55f53b6f9a991b32b060
wplude114 <wplude@icloud.com>
2026-06-23 08:02:21 -0500

    Remove sentry minion classes from projectiles
    Removed sentry minion definitions including sentrySwarm, sentryGun, and sentryTrap.

--- server/modules/definitions/groups/projectiles.js
+++ server/modules/definitions/groups/projectiles.js
@@ -537,39 +537,6 @@ Class.tinyMinion = {
     DIE_AT_RANGE: true,
     BUFF_VS_FOOD: true,
 }
-Class.sentrySwarmMinion = {
-    PARENT: 'drone',
-    LABEL: 'sentry',
-    COLOR: 'pink',
-    UPGRADE_COLOR: "pink",
-    DRAW_HEALTH: true,
-    HAS_NO_RECOIL: true,
-    GUNS: Class.sentrySwarm.GUNS
-}
-Class.sentryGunMinion = {
-    PARENT: 'drone',
-    LABEL: 'sentry',
-    COLOR: 'pink',
-    UPGRADE_COLOR: "pink",
-    DRAW_HEALTH: true,
-    HAS_NO_RECOIL: true,
-    TURRETS: [{
-        POSITION: [12, 0, 0, 0, 360, 1],
-        TYPE: ['megaAutoTankGun', {GUN_STAT_SCALE: {health: 0.8}}]
-    }]
-}
-Class.sentryTrapMinion = {
-    PARENT: 'drone',
-    LABEL: 'sentry',
-    COLOR: 'pink',
-    UPGRADE_COLOR: "pink",
-    DRAW_HEALTH: true,
-    HAS_NO_RECOIL: true,
-    TURRETS: [{
-        POSITION: [12, 0, 0, 0, 360, 1],
-        TYPE: 'trapTurret'
-    }]
-}
 
 // Traps
 Class.setTrap = {

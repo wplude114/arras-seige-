77853c794888e63dbd4cc07717e679663e99f2df
wplude114 <wplude@icloud.com>
2026-06-23 22:53:34 -0500

    Change loop limit from 8 to 6 for tiers

--- server/modules/definitions/groups/food.js
+++ server/modules/definitions/groups/food.js
@@ -242,7 +242,7 @@ Class.tetradecagon = {
 
 let polyNames = [ "triangle", "square", "pentagon", "hexagon", "septagon", "octagon", "nonagon", "decagon", "undecagon", "dodecagon", "tridecagon" ]
 const tierNames = [ null, 'beta', 'alpha', 'gamma', 'delta', 'zeta' ];
-for (let tier = 1; tier < 8; tier++) {
+for (let tier = 1; tier < 6; tier++) {
     for (const polyLower of polyNames) {
 
         const food = polyLower;

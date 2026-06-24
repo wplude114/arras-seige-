89ee434b9b3c0c4811993d81c28074b0372b00c6
wplude114 <wplude@icloud.com>
2026-06-23 22:44:45 -0500

    Fix LABEL formatting in facilitators.js

--- server/modules/definitions/facilitators.js
+++ server/modules/definitions/facilitators.js
@@ -1032,7 +1032,7 @@ exports.makeLaby = (type, level, baseScale = 1) => {
         strengthMultiplier = 5 ** level;
     return {
         PARENT: "food",
-        LABEL: ["", "Beta ", "Alpha ", "Gamma ", "Delta ", "Epsilon ", "Zeta ", "Eta ", "Theta"][level] + type.LABEL,
+        LABEL: "[" + ["", "Beta ", "Alpha ", "Gamma ", "Delta ", "Epsilon ", "Zeta ", "Eta ", "Theta"][level] + "] " + type.LABEL,
         VALUE: type.VALUE * strengthMultiplier,
         SHAPE: type.SHAPE,
         SIZE: type.SIZE * baseScale / downscale ** level,

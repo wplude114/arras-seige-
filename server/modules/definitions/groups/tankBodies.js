34dd101fa93caf576f813242746ef23a23b0acfe
wplude114 <wplude@icloud.com>
2026-06-24 05:51:10 -0500

    Remove train segment, bolt, and body definitions

--- server/modules/definitions/groups/tankBodies.js
+++ server/modules/definitions/groups/tankBodies.js
@@ -81,51 +81,6 @@ Class.thrusterBody = {
     ],
 }
 
-Class.trainSegment = {
-     PARENT: "genericEntity",
-    LABEL: "Body",
-    INDEPENDENT: true,
-    COLOR: "mirror",
-    
-    SHAPE: [[-1.7,-1],[0,-1.3],[1.7,-1],[1.7,1],[0,1.3],[-1.7,1]],
-    GUNS: [
-      {
-            POSITION: {LENGTH: 15,WIDTH: 31,ASPECT: 1, X: 0,Y: 0,ANGLE: 90,DELAY: 0},
-            PROPERTIES: {
-              COLOR: 17
-            }
-        },
-      {
-            POSITION: {LENGTH: 15,WIDTH: 31,ASPECT: 1, X: 0,Y: 0,ANGLE: -90,DELAY: 0},
-            PROPERTIES: {
-              COLOR: 17
-            }
-        },
-    ],
-    TURRETS: [{
-        POSITION: [30, 0, 0, 0, 360, 1],
-        TYPE: [ "barrel" ]
-    }],
-}
-Class.trainBolt = {
-    PARENT: "genericTank",
-    COLOR: "grey",
-    INDEPENDENT: true,
-    GUNS: [{
-        POSITION: [50, 5, 1, 8, 0, 0, 0]
-    }],
-    TURRETS: [{
-        POSITION: [40, 50, 0, 0, 360, 1],
-        TYPE: "trainSegment"
-    }],
-}
-Class.trainBody = {
-    PARENT: "tankBody",
-    TURRETS: [
-        { POSITION: [10, 15, 0, 180, 180, -10], TYPE: "trainBolt" }
-    ]
-}
-
 // aura stuff goes here
 Class.genericBase = { PARENT: "genericTank", LABEL: null,  }
 Class.Base = { PARENT: "genericBase", UPGRADE_LABEL: "Base", TURRETS: [{ POSITION: [17, 0, 0, 0, 360, -10], TYPE: "tankBody" }] }
@@ -136,6 +91,6 @@ Class.BaseD = { PARENT: "Base" }
 
     Class.Train = { PARENT: "genericBase", UPGRADE_LABEL: "Body", TURRETS: [{ POSITION: [17, 0, 0, 0, 360, -10], TYPE: "trainBody" }] }
 
-Class.Base.UPGRADES_TIER_2 = ["Booster",["BaseD","Train"]]
+Class.Base.UPGRADES_TIER_2 = ["Booster"]
 Class.BaseD.UPGRADES_TIER_2 = ["Booster"]
     Class.Booster.UPGRADES_TIER_3 = ["Thruster", "Resurfacer"]

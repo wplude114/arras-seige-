ebb11536ec91e56735963c50148ce018a921e3be
wplude114 <wplude@icloud.com>
2026-07-06 16:16:32 -0500

    Update permissions with new roles and notes
    Added new permission entries for spectator and whirlwind classes.

--- server/permissions.js
+++ server/permissions.js
@@ -7,5 +7,23 @@ module.exports = [
         "infiniteLevelUp": true,
         "name": "unnamed#0000",
         "note": "Hi"
-    }
+    },
+    {
+        "key": process.env.TOKEN_2,
+        "discordID": "0",
+        //"nameColor": "#ebab57",
+        "class": "spectator",
+        "infiniteLevelUp": false,
+        "name": "unnamed#0000",
+        "note": "Enables Spectator mode"
+    },
+    {
+        "key": process.env.TOKEN_3,
+        "discordID": "0",
+        //"nameColor": "#ebab57",
+        "class": ["whirlwind","Base"],
+        "infiniteLevelUp": false,
+        "name": "unnamed#0000",
+        "note": "WHIRLWIND"
+    },
 ]

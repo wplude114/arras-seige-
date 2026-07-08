98778234f9eee932a8fa4942e4cc23a2fb63bb66
wplude114 <wplude@icloud.com>
2026-07-08 07:12:52 -0500

    Modify gun position dimensions and coordinates
    Updated gun positions with new dimensions and coordinates.

--- server/modules/definitions/facilitators.js
+++ server/modules/definitions/facilitators.js
@@ -925,10 +925,10 @@ exports.makeSanc = (type) => {
 		LABEL: "PLACEHOLDER NAME", // grr i cant name things!!
 		GUNS: exports.weaponArray([
 			 {
-            POSITION: {LENGTH: 7,WIDTH: 10,ASPECT: 1, X: 13,Y: 0,ANGLE: 0,DELAY: 0},
+            POSITION: {LENGTH: 15,WIDTH: 6,ASPECT: 1, X: 0,Y: 0,ANGLE: 0,DELAY: 0},
         },
         {
-            POSITION: {LENGTH: 4,WIDTH: 10,ASPECT: 1.4, X: 20,Y: 0,ANGLE: 0,DELAY: 0},
+            POSITION: {LENGTH: 3,WIDTH: 6,ASPECT: 1.4, X: 15,Y: 0,ANGLE: 0,DELAY: 0},
             PROPERTIES: {
                 SHOOT_SETTINGS: exports.combineStats([g.basic, {range: 0.3, damage: 0.3, recoil: 3, shudder: 10, speed: 0.5}]),
                 TYPE: "trap",

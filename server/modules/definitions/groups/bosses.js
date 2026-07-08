57f796155b24e3010d4a7916bb3846b8c696e3bf
wplude114 <wplude@icloud.com>
2026-07-08 15:51:58 -0500

    Refactor bosses.js to update boss definitions

--- server/modules/definitions/groups/bosses.js
+++ server/modules/definitions/groups/bosses.js
@@ -62,15 +62,11 @@ Class.ravager = {
     
     TURRETS: turretArray([
         {
-            POSITION: [7, 9, 0, 0, 150, -1],
+            POSITION: [4, 9, 0, 0, 150, -1],
             TYPE: ["autoMachineGun", {INDEPENDENT: false, HAS_NO_RECOIL: true}]
         },
     ],5)
 };
-Class.ravager.TURRETS.push([{
-    POSITION: [10, 9, 0, 0, 150, 5],
-    TYPE: ["autoTankGun", {INDEPENDENT: false, HAS_NO_RECOIL: true}]
-}]);
 
 // menu stuff (i dont want to edit dev.js every time i add something)
 if (!Class.bosses) Class.bosses = {}; if (!Array.isArray(Class.bosses.UPGRADES_TIER_0)) Class.bosses.UPGRADES_TIER_0 = []; // make sure it exists

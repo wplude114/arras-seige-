5b52e0a4f094e0b6152733ce342fb7cfa523e08e
wplude114 <wplude@icloud.com>
2026-07-09 16:43:13 -0500

    Add minimapEnabled variable to app.js

--- public/app.js
+++ public/app.js
@@ -10,6 +10,7 @@ import * as socketStuff from "./lib/socketInit.js";
 (async function (util, global, settings, Canvas, color, gameDraw, socketStuff) {
 const gameName = 'Project Solace';
 const nameColor = '#888';
+const minimapEnabled = true; // this dont work yet
 let { socketInit, gui, leaderboard, minimap, moveCompensation, lag, getNow } = socketStuff;
 // fetch("changelog.md", { cache: "no-cache" })
 // .then((response) => response.text())

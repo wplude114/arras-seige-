d5fa3cf38e6f95b363febeedaf06f7c578a7fbaa
wplude114 <wplude@icloud.com>
2026-07-09 17:16:24 -0500

    Adjust globalAlpha for drawing transparency

--- public/app.js
+++ public/app.js
@@ -1221,7 +1221,11 @@ function drawEntityIcon(model, x, y, len, height, lineWidthMult, angle, alpha, c
         ? gameDraw.modifyColor(picture.upgradeColor)
         : gameDraw.getColor(getIconColor(colorIndex));
     let bigRadius = 5; // adjust percent to taste
+    ctx.globalAlpha = 0.7;
     drawRoundedRect(x, y, len, height, bigRadius, false);
+    ctx.globalAlpha = 0.1;
+    ctx.fillStyle = color.black;
+    drawRoundedRect(x, y*1.4, len, height*0.6, bigRadius, false);
 
     // Shading for hover
     if (hover) {

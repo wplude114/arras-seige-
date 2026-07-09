03eb45b3cd67264bd5462b0e9619f75ff9a234ac
wplude114 <wplude@icloud.com>
2026-07-09 14:11:16 -0500

    Fix turret position and type definitions in tankBodies

--- server/modules/definitions/groups/tankBodies.js
+++ server/modules/definitions/groups/tankBodies.js
@@ -148,9 +148,9 @@ Class.Base = { PARENT: "genericBase", UPGRADE_LABEL: "Base", TURRETS: [{ POSITIO
             UPGRADE_LABEL: "Mechanism",
             TURRETS: [
                 { POSITION: [17, 0, 0, 0, 360, -10], TYPE: "tankBody" },
-                {POSITION: [6,-5,0,0,360,15], TYPE: ["autoTankGun", {INDEPENDENT: true, HAS_NO_RECOIL: true}]},
-                {POSITION: [6,-5,0,120,360,15], TYPE: ["autoTankGun", {INDEPENDENT: true, HAS_NO_RECOIL: true}]},
-                {POSITION: [6,-5,0,-120,360,15], TYPE: ["autoTankGun", {INDEPENDENT: true, HAS_NO_RECOIL: true}]},
+                {POSITION: [6,5,0,60,360,15], TYPE: ["autoTankGun", {INDEPENDENT: true, HAS_NO_RECOIL: true}]},
+                {POSITION: [6,5,0,120+60,360,15], TYPE: ["autoTankGun", {INDEPENDENT: true, HAS_NO_RECOIL: true}]},
+                {POSITION: [6,5,0,-120-60,360,15], TYPE: ["autoTankGun", {INDEPENDENT: true, HAS_NO_RECOIL: true}]},
             ]
         }
         Class.Tracker = { PARENT: "genericBase", UPGRADE_LABEL: "Tracker", TURRETS: [{ POSITION: [17, 0, 0, 0, 360, -10], TYPE: "tankBody" }, {POSITION: [7,0,0,180,360,15], TYPE: ["autoTankTracker", {INDEPENDENT: true}]}] }

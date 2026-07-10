bab137e63c9bccb12b050fda51a1b8310a44d16d
wplude114 <wplude@icloud.com>
2026-07-10 13:07:47 -0500

    Change class property to a single value

--- server/permissions.js
+++ server/permissions.js
@@ -3,7 +3,7 @@ module.exports = [
         "key": process.env.TOKEN_1,
         "discordID": "0",
         "nameColor": "#ebab57",
-        "class": ["developer", "Base"],
+        "class": "developer",
         "infiniteLevelUp": true,
         "name": "unnamed#0000",
         "note": "Hi"

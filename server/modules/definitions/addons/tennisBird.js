e51a8627b18a3a6bb7ad8a8e23253869327a9fa3
wplude114 <wplude@icloud.com>
2026-07-09 22:06:42 -0500

    Add tennisBird class definition and upgrade tier

---
+++ server/modules/definitions/addons/tennisBird.js
@@ -0,0 +1,11 @@
+const { combineStats, menu, menuvar2, addAura, makeDeco, LayeredBoss, newWeapon, weaponArray, makeRadialAuto, makeTurret } = require('../facilitators.js');
+const { base, basePolygonDamage, basePolygonHealth, dfltskl, statnames } = require('../constants.js');
+const g = require('../gunvals.js');
+
+Class.tennisBird = {
+	PARENT: "genericTank",
+	SHAPE: "https://cdn.discordapp.com/emojis/1447472307663802479.webp?size=40",// offical emoji in arrascord, wont break anytime soon :)
+	HITS_OWN_TYPE: 'hard',
+}
+
+Class.addons.UPGRADES_TIER_0.push("tennisBird");

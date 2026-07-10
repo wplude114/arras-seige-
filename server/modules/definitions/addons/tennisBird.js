75957f991213d9e1099e2b9c9970eacf666fc460
wplude114 <wplude@icloud.com>
2026-07-09 22:11:19 -0500

    Add LABEL and UPGRADE_TOOLTIP to tennisBird class
    Updated tennisBird class to include LABEL and UPGRADE_TOOLTIP properties.

--- server/modules/definitions/addons/tennisBird.js
+++ server/modules/definitions/addons/tennisBird.js
@@ -4,7 +4,9 @@ const g = require('../gunvals.js');
 
 Class.tennisBird = {
 	PARENT: "genericTank",
-	SHAPE: "https://cdn.discordapp.com/emojis/1447472307663802479.webp?size=40",// offical emoji in arrascord, wont break anytime soon :)
+	LABEL: "Tennis Bird",
+	UPGRADE_TOOLTIP: '"1447472307663802479.webp"',
+	SHAPE: "https://cdn.discordapp.com/emojis/1447472307663802479.webp?size=40", // offical emoji in arrascord, wont break anytime soon :)
 	HITS_OWN_TYPE: 'hard',
 }
 
